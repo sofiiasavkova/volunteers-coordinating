e48d61b6ca01378839e2756fefe0057b5e2d92a5
Соня Cавкова <silva.vvss12@gmail.com>
2024-12-03 08:10:11 +0200

    added listviews and templates for them

--- coordination/urls.py
+++ coordination/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from . import views
+from .views import (
+    VolunteerListView,
+    ProjectListView,
+    TaskListView,
+    CoordinatorListView
+)
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('volunteers/', VolunteerListView.as_view(), name='all_volunteers'),
+    path('projects/', ProjectListView.as_view(), name='all_projects'),
+    path('tasks/', TaskListView.as_view(), name='all_tasks'),
+    path('coordinators/', CoordinatorListView.as_view(), name='all_coordinators'),
 ]
--- coordination/views.py
+++ coordination/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
+from .models import Volunteer, Project, Task, Coordinator
+from django.views.generic import ListView
 
 def home(request):
     return render(request, 'coordination/home.html')
+
+class CoordinatorListView(ListView):
+    model = Coordinator
+    template_name = 'coordination/coordinators_list.html'
+    context_object_name = 'coordinators'
+
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = 'coordination/projects_list.html'
+    context_object_name = 'projects'
+
+
+class VolunteerListView(ListView):
+    model = Volunteer
+    template_name = 'coordination/volunteers_list.html'
+    context_object_name = 'volunteers'
+
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'coordination/tasks_list.html'
+    context_object_name = 'tasks'
+
