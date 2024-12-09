5a3fd19318c2d4093d122f9a7a44d26a326722ba
Соня Cавкова <silva.vvss12@gmail.com>
2024-12-09 13:59:53 +0200

    added render domain to ALLOWED_HOSTS

--- volunteers_coordinating/settings/prod.py
+++ volunteers_coordinating/settings/prod.py
@@ -5,7 +5,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "volunteers-coordinating.onrender.com"]
 
 
 # Database
