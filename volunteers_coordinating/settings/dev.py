c4331e67cf81f8594e3e5ef5e8f0388e7f07b993
Соня Cавкова <silva.vvss12@gmail.com>
2024-12-09 09:19:18 +0200

    changes

--- volunteers_coordinating/settings/dev.py
+++ volunteers_coordinating/settings/dev.py
@@ -3,7 +3,7 @@ import os
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
